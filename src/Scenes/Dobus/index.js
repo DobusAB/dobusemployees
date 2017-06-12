e622df27601715eb7a4f8ab3f831eb84e7d2567e
johandobus <johan@dobus.se>
2017-06-12 15:24:09 +0200

    fixes

--- src/Scenes/Dobus/index.js
+++ src/Scenes/Dobus/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DobusImage from '../../Components/DobusImage';
+import './style.scss';
 
 class Dobus extends Component {
 
@@ -23,12 +24,35 @@ class Dobus extends Component {
     render() {
        
         return (
-            <div className="dobus">
-                <div className="container">
-                    <DobusImage />
+            <div>
+                <div className="dobus">
+                    <div className="container">
+                        <DobusImage />
+                    </div>
+                </div>
+                <div className="dobusdescription">
+                    <div className="container">
+                        <div className="columns">
+                            <div className="column is-12">
+                                <h2>Så, vad är då Dobus och vad gör de? </h2>
+                                <p>En gång en webbyrå, inte alltid en webbyrå. Dobus grundades år 2015 som en webbyrå med utvecklarkompetens endast. </p>
+                                <p>I dag är Dobus en snabbt växande digital byrå med fokus på mycket mer än bara webb.  Vi skräddarsyr och designar helhetslösningar som har användaren i åtanke, för att därefter påbörja utvecklingsprocssen.  </p>
+                                <p>Nyckelorden är användarcentrerad och iterativ utveckling,  saker som vi tror gör skillnaden mellan att nå ett bra och mindre bra resultat.  </p>
+                                <div className="greetings">
+                                    <h4>Hälsningar från gänget på <span className="strong">Dobus</span>.</h4>
+                                    <div className="is-flex">
+                                        <img className="image is-48x48" src="/johan.png" />
+                                        <img className="image is-48x48" src="/tim.png" />
+                                        <img className="image is-48x48" src="/viman.png" />
+                                        <img className="image is-48x48" src="/alice.png" />
+                                        <img className="image is-48x48" src="/tobias.png" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            
         );
     }
 }
