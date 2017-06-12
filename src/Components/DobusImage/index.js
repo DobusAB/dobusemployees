9bb9d5145a5a1d0affb0326d75d39c4e028ee885
timdobus <Arbetskonto@Tims-MacBook-Pro-2.local>
2017-06-12 15:52:57 +0200

    Fix med animations

--- src/Components/DobusImage/index.js
+++ src/Components/DobusImage/index.js
@@ -8,7 +8,7 @@ const DobusImage = () => {
         <div>
         <div className="columns">
             <div className="column is-12">
-                <h2 className="has-text-centered">Hej Husqvarna.  Vi är Dobus. <br /> En ung och växande digital byrå med säte i Halmstad.</h2>
+                <h2 className="has-text-centered"><span className="fade-in one">Hej Husqvarna.</span><span className="fade-in three">Vi är Dobus.</span> <br /> <span className="fade-in five">En ung och växande digital byrå med säte i Halmstad.</span></h2>
             </div>
         </div>
         <div className="dobus-image">
