3d9c99cca701818f8730db26a2c7f6c51a6c3fe8
johandobus <johan@dobus.se>
2017-06-12 14:44:31 +0200

    fixes

--- src/Scenes/Dobus/index.js
+++ src/Scenes/Dobus/index.js
@@ -34,6 +34,23 @@ class Dobus extends Component {
                             <h2 className="has-text-centered" style={this.state.styles}>Hej Husqvarna.  Vi är Dobus. <br /> En ung och växande digital byrå med säte i Halmstad.</h2>
                         </div>
                     </div>
+                    <div className="dobus-image">
+                        <div className="columns has-text-centered">
+                            <div className="column is-12 ">
+                                <img className="site"  src="/dobussite.png"/>
+                            </div>
+                        </div>
+                        <div className="columns scrolldown">
+                            <div className="column is-12">
+                                <div className="has-text-centered">
+                                    <div className="has-text-centered">
+                                        <img className=""  src="/mouseicon.svg"/>
+                                        <p>Scrolla ner</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
                 </div>
             </div>
             
