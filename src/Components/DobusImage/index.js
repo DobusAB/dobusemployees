1b95c1ce14d2224a3c5435e20a682208be07fe27
johandobus <johan@dobus.se>
2017-06-12 15:00:16 +0200

    fixes

---
+++ src/Components/DobusImage/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const DobusImage = () => {
+
+    return (
+        <div>
+        <div className="columns">
+            <div className="column is-12">
+                <h2 className="has-text-centered">Hej Husqvarna.  Vi är Dobus. <br /> En ung och växande digital byrå med säte i Halmstad.</h2>
+            </div>
+        </div>
+        <div className="dobus-image">
+            <div className="columns has-text-centered">
+                <div className="column is-12 ">
+                    <img className="site"  src="/dobussite.png"/>
+                </div>
+            </div>
+            <div className="columns scrolldown">
+                <div className="column is-12">
+                    <div className="has-text-centered">
+                        <div className="has-text-centered">
+                            <img className=""  src="/mouseicon.svg"/>
+                            <p>Scrolla ner</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default DobusImage;
--- src/Scenes/Dobus/index.js
+++ src/Scenes/Dobus/index.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import Memories from '../../Components/Firstmemory';
-import Skills from '../../Components/Skills';
-import What from '../../Components/What';
-import Footer from '../../Components/Footer';
+import DobusImage from '../../Components/DobusImage';
 import './style.scss';
 
 class Dobus extends Component {
@@ -29,28 +26,7 @@ class Dobus extends Component {
         return (
             <div className="dobus">
                 <div className="container">
-                    <div className="columns">
-                        <div className="column is-12">
-                            <h2 className="has-text-centered" style={this.state.styles}>Hej Husqvarna.  Vi är Dobus. <br /> En ung och växande digital byrå med säte i Halmstad.</h2>
-                        </div>
-                    </div>
-                    <div className="dobus-image">
-                        <div className="columns has-text-centered">
-                            <div className="column is-12 ">
-                                <img className="site"  src="/dobussite.png"/>
-                            </div>
-                        </div>
-                        <div className="columns scrolldown">
-                            <div className="column is-12">
-                                <div className="has-text-centered">
-                                    <div className="has-text-centered">
-                                        <img className=""  src="/mouseicon.svg"/>
-                                        <p>Scrolla ner</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                     </div>
+                    <DobusImage />
                 </div>
             </div>
             
