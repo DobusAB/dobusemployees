a98da7db8359cafbb1b753e6028cb6eb0613f86f
johandobus <johan@dobus.se>
2017-07-21 14:00:24 +0200

    fix

--- src/Scenes/Dobus/index.js
+++ src/Scenes/Dobus/index.js
@@ -42,6 +42,12 @@ class Dobus extends Component {
                                 <h2>En gång en webbyrå, nu en digital byrå.</h2>
                                 <p>Dobus grundades år 2015 som en webbyrå med utvecklarkompetens endast. I dag är Dobus en snabbt växande <span className="yellow strong">digital byrå</span> med fokus på <span className="yellow strong">mycket mer än bara webb.</span>  Vi skräddarsyr och designar helhetslösningar, för att därefter påbörja en utvecklingsprocess där kundkommunikation värdesätts högt.  </p>
                                 <p>Nyckelorden är <span className="yellow strong">användarcentrerad</span> och <span className="yellow strong">iterativ utveckling</span>,  saker som vi tror gör skillnaden när det kommer till att nå ett bra resultat.</p>
+                                { /* <h2 className="no-margin">Några av våra kunder vi fått förtroende av: </h2>
+                                <div className="columns">
+                                    <div className="column is-6">
+                                        <p><span className="tag is-light is-large"> Hemsida 24 AB </span> <span className="tag is-light is-large"> Brandskydd & HLR i Halland AB</span> <span className="tag is-light is-large">Riddermarkbil AB </span> <span className="tag is-light is-large">Bendt bil AB </span>  <span className="tag is-light is-large">Marknadsföreningen i Halland </span> <span className="tag is-light is-large"> Aktiebolaget Halmstad Kylteknik </span> <span className="tag is-light is-large">Forefront Consulting Group AB </span></p>
+                                    </div>
+                                </div> */ }
                                 <div className="greetings">
                                     <h4>Hälsningar från gänget på <span className="strong">Dobus</span>.</h4>
                                     <div className="is-flex">
